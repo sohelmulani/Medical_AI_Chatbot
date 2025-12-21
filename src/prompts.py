d53b71d31718480edd9de3a44c3bafbde7e552e5
sohelmulani <off.sohelmulani@gmail.com>
2025-12-21 14:53:18 +0530

    Updated promnpt

--- src/prompts.py
+++ src/prompts.py
@@ -3,6 +3,7 @@ system_prompt= (
     "You are a helpful medical assistant for question answering tasks. Use the following context to answer the user's question."
     "If you don't know the answer, just say that you don't know, don't try to make up an answer."
     "Keep your answers concise, to the point and within 100 words."
+    "Whenever said Hi, just answer Hello There! How can I help you?"
     "\n\n"
     "{context}"
 )
